efd27a931cf2add2fe93f85f7d9b422c6bcb7dbe
Rosie-Brigham <rosiebrigham@gmail.com>
2021-04-19 15:54:25 +0100

    adapt predictive plots

--- predictivePlots.py
+++ predictivePlots.py
@@ -1,5 +1,14 @@
 from base import *
 
+CSVFilename = 'predictions/1981-1990.csv' ### <<<<< Change this file when needed
+p1981 = pd.read_csv(CSVFilename, index_col=False)
+
+CSVFilename = 'predictions/1991-2000.csv' ### <<<<< Change this file when needed
+p1991 = pd.read_csv(CSVFilename, index_col=False)
+
+CSVFilename = 'predictions/2001-2010.csv' ### <<<<< Change this file when needed
+p2001 = pd.read_csv(CSVFilename, index_col=False)
+
 CSVFilename = 'predictions/2011-2020.csv' ### <<<<< Change this file when needed
 p2011 = pd.read_csv(CSVFilename, index_col=False)
 
@@ -20,3 +29,28 @@ p2061 = pd.read_csv(CSVFilename, index_col=False)
 
 CSVFilename = 'predictions/2071-2080.csv' ### <<<<< Change this file when needed
 p2071 = pd.read_csv(CSVFilename, index_col=False)
+
+CSVFilename = 'predictions/measured-2011-2020.csv' ### <<<<< Change this file when needed
+measured = pd.read_csv(CSVFilename, index_col=False)
+
+# Get all teh value counts together to compare
+data = {
+  '1981':  p1981['mode'].value_counts(),
+  '1991':  p1991['mode'].value_counts(),
+  '2001':  p2001['mode'].value_counts(),
+  '2011':  p2011['mode'].value_counts(),
+  '2021':  p2021['mode'].value_counts(),
+  '2031':  p2031['mode'].value_counts(),
+  '2041':  p2041['mode'].value_counts(),
+  '2051':  p2051['mode'].value_counts(),
+  '2061':  p2061['mode'].value_counts(),
+  '2071':  p2071['mode'].value_counts(),
+}
+
+df = pd.DataFrame(data)
+
+
+import code; code.interact(local=dict(globals(), **locals()))
+# Split ranks b season to compare
+
+
